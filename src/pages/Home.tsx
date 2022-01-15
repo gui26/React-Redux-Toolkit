85c0959c392da57763634142dd16732f25b0310f
gui26 <guilhermeb20@gmail.com>
2022-01-15 15:46:57 -0300

    melhorias adicionadas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import{ useDispatch, useSelector} from 'react-redux';
+import { Getusers, usersselected } from "../store/userSlice";
 
 export const Home = () =>{
+const { users } = useSelector(usersselected)
+    const dispatch = useDispatch();
+
+
+useEffect( ()=>{
+    dispatch(Getusers());
+
+},[dispatch])
+
+
+
+
     return(
         <div>
+            { users && users.map((user:any, i:number)=><h1 key={i} className="Title">{user.name}</h1>) } 
             <h1>Aqui é Home</h1>
         </div>
     )
